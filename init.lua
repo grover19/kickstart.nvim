a042c26a681b969dcb52f1cab1aca56597b05ec4
grover19 <ag8266@nyu.edu>
2026-02-01 13:49:40 -0800

    added lua line and cleaned up config a bit

--- init.lua
+++ init.lua
@@ -217,6 +217,7 @@ require('lazy').setup({
         highlights = {
           CursorLineNr = { bg = 'NONE' },
           CursorLine = { bg = 'NONE' },
+          Visual = { bg = 'grey' },
         },
       }
       vim.cmd 'colorscheme onedark_dark'
@@ -261,6 +262,10 @@ require('lazy').setup({
     },
   },
 
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -853,30 +858,6 @@ require('lazy').setup({
     },
   },
 
-  --[[
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
---]]
-  --
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
