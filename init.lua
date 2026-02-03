b4391c153e515ac73d873c3d163cea62d79b2d20
grover19 <ag8266@nyu.edu>
2026-02-02 23:24:39 -0800

    switching off relative line number

--- init.lua
+++ init.lua
@@ -61,7 +61,7 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.o.number = true
-vim.o.relativenumber = true
+-- vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
